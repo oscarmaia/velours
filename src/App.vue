677ea85d10dae02358334f8231e23d91c1c08a57
oscarfgmaia <oscarfgmaia@gmail.com>
2023-06-26 08:10:14 -0300

    project started

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <nav
+      id="nav"
+      class="container py-2 mx-2 md:mx-auto self-center mb-10 h-10 flex justify-between"
+    >
+      <div>Velours</div>
+      <div class="flex gap-4">
+        <RouterLink id="client" to="/">Client</RouterLink>
+        <RouterLink id="admin" to="/inventory">Admin</RouterLink>
+      </div>
+      <div>Profile</div>
+    </nav>
+  </header>
+  <RouterView />
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/HomePage.vue'
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/inventory',
+      name: 'inventory',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/InventoryPage.vue')
+    }
+  ]
+})
+
+export default router
