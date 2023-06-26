586a54a86f369abfc3b4e8faa37846e2581b2173
oscarfgmaia <oscarfgmaia@gmail.com>
2023-06-26 17:22:31 -0300

    feat: add new item page

--- src/router/index.js
+++ src/router/index.js
@@ -11,10 +11,12 @@ const router = createRouter({
     {
       path: '/admin',
       name: 'admin',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AdminPage.vue')
+    },
+    {
+      path: '/admin/add',
+      name: 'add',
+      component: () => import('../views/AddItem.vue')
     }
   ]
 })
