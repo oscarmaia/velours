5d3a212e4dbde7af7ef1eceae74843b947f5e96f
oscarfgmaia <oscarfgmaia@gmail.com>
2023-06-26 09:34:53 -0300

    feat: add admin page

--- src/views/AdminPage.vue
+++ src/views/AdminPage.vue
@@ -1,7 +1,27 @@
 <template>
-
+  <div class="h-screen w-screen fixed z-50 top-0 left-0 flex items-center justify-center">
+    <div class="w-4/6  h-4/6 bg-red-300">
+      <h1 class="text-center">CENTRO DA TELA</h1>
+    </div>
+  </div>
+  <div>
+    <h1 class="text-center text-2xl font-bold my-4">ADMIN PAGE</h1>
+  </div>
+  <ProductList class="my-4" :admin="admin"></ProductList>
 </template>
 
-<script setup>
-
+<script>
+import itens from "../assets/repository";
+import ProductList from "../components/ProductList.vue";
+export default {
+  data() {
+    return {
+      itens,
+      admin: true,
+    };
+  },
+  components: {
+    ProductList,
+  },
+};
 </script>
