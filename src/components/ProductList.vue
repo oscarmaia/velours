c0f5fce750947e2f728e085270b66b64bae98c82
oscarfgmaia <oscarfgmaia@gmail.com>
2023-06-26 17:22:16 -0300

    feat: fetch data before loading

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -3,21 +3,23 @@
     class="grid place-items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 gap-4"
   >
     <Product
-      v-for="(item, index) in itens"
+      v-for="(item, index) in arr"
       :name="item.name"
       :price="item.price"
       :quantity="item.quantity"
       :image="item.image"
       :discount="item.discount"
       :key="index"
+      :id="index"
       :admin="admin"
+      @event-from-item="sendDirectlyToAdmin"
     ></Product>
   </div>
 </template>
 
 <script>
 import Product from "./Product.vue";
-import itens from "../assets/repository";
+import { getItens } from "../assets/repository";
 export default {
   props: {
     admin: Boolean,
@@ -25,8 +27,22 @@ export default {
   components: {
     Product,
   },
+
   data() {
-    return { itens };
+    return {
+      arr: [],
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      this.arr = getItens();
+    },
+    sendDirectlyToAdmin(data) {
+      this.$emit("send-to-admin", data);
+    },
   },
 };
 </script>
